ffc5c5d1f59dca8df76ee7997bfb89b30301ef3f
MNGARCIA085 <mngarcia085@gmail.com>
2022-09-27 01:14:54 +0200

    GET launches

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
  
 const app = express();
 
@@ -17,10 +18,12 @@ app.use(express.json());
 app.use(express.static(path.join('__dirname','..','public')));
  
 app.use(planetsRouter);
+app.use(launchesRouter);
 
-app.get('/', (req,res) => {
+app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname,'..','public','index.html'));
 })
+
 
  
 module.exports = app;
