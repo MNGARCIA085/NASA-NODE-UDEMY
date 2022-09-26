2659b27547721f20b05a1f9adeaff21d330f2cba
MNGARCIA085 <mngarcia085@gmail.com>
2022-09-27 00:46:29 +0200

    agrego logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
  
@@ -8,6 +9,9 @@ const app = express();
 
 // solucionando el eror de cors
 app.use( cors ( {origin: 'http://localhost:3000' }) ); 
+
+// va tan alto como se pueda
+app.use(morgan('combined'));
  
 app.use(express.json());
 app.use(express.static(path.join('__dirname','..','public')));
